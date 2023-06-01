8df2c3b673cac286b3c47892bb771f61daa5f6bc
anoj1812 <anojaruldhas18@gmail.com>
2023-06-01 15:24:33 +0530

    Initial commit

---
+++ src/main/java/com/techgeeknext/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.techgeeknext.repository;
+
+import com.techgeeknext.model.UserDao;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserDao, Integer> {
+    UserDao findByUsername(String username);
+}
